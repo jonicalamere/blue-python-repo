8e748a9c678f82dcc42bb4038c329603ba51ebdf
Jonica Lamere <jonicalamere@gmail.com>
2023-06-08 23:46:00 +0000

    Coursework week 12

---
+++ Practing printg.py
@@ -0,0 +1,4 @@
+print("I'm learning to become a Python developer!")
+print("I'm so excited!")
+print('I\'m learning to become a Python developer!\nI\'m so excited!')
+print('I\'m learning to become a Python developer!' 'I\'m so excited!')
