75372c7b3f2b4aa88ad97c2a719044953953ffbc
Jonica Lamere <jonicalamere@gmail.com>
2023-06-15 00:39:55 +0000

    sort_random_positions

---
+++ Module4_budget_exercise.py
@@ -0,0 +1,6 @@
+spendings = [32.45, 18.65, 23.45, 78.32, 5.23]
+sum = 0.0
+for spending in spendings:
+    sum += spending
+print('Money spent:', sum)
+
---
+++ sort-top_cities-positions.py
@@ -0,0 +1,3 @@
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+print(sorted(top_cities))
+print(top_cities)
