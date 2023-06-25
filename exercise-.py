844ea51299d62121410d72ad1b7d8de9cedefa72
Jonica Lamere <jonicalamere@gmail.com>
2023-06-25 20:22:54 +0000

    Python

---
+++ exercise-.py
@@ -0,0 +1,12 @@
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+
+print('Your income is', income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You would save', income * ripoffland_rate - income * lowtaxland_rate, 'by paying taxes in Lowtaxland!')
+
+login = input('Enter your login: ')
+language = input('Enter your native language: ')
+print('Your login is', login, 'and you speak', language)
+
+
+
---
+++ module1-numerical-representation-.py
@@ -0,0 +1,12 @@
+# underscores in numbers
+12000300
+12_000_300
+#scientific notation
+# 3e4 = 3E4 = 3 * 10000 = 3000
+#3e-4 = 3E-4 = 3 * 1/1000 = 0.0003
+print (0.000000000000000003)
+
+
+# hexadeciml numbers
+# start with 0X or 0x
+print (0x123)
